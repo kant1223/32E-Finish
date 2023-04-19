b3592d19886eb4bea71886b43d6a3fea5561cd8b
kant1223 <131114390+kant1223@users.noreply.github.com>
2023-04-19 20:22:12 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,30 +1,37 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-from flask import Flask
+access_token = '8zp872p8zjIBjP89c4dtoPewB239ZuOuA62gIShbHcMuBBvssLb8f2fep9rFmVMGh5TBVgoo55L7S0KDYzNgy0+NGvmieYfx7DwunGMlmKefVPjWo0MPrpS3mMDCoAagqJCHIhbKoTRMjbbhx8BTKwdB04t89/1O/w1cDnyilFU='
+secret = '575a9bb8ae3a9aa680e0dfb69f5f2750'
 app = Flask(__name__)
-@app.route("/")
-def hello():
-    return "Hello, World!"
 
+@app.route("/", methods=['POST'])
+def linebot():
+    body = request.get_data(as_text=True)
+    json_data = json.loads(body)
+    print(json_data)
+    try:
+        line_bot_api = LineBotApi(access_token)
+        handler = WebhookHandler(secret)
+        signature = request.headers['X-Line-Signature']
+        handler.handle(body, signature)
+        tk = json_data['events'][0]['replyToken']         # 取得 reply token
+        msg = json_data['events'][0]['message']['text']   # 取得使用者發送的訊息
+        text_message = TextSendMessage(text=msg)          # 設定回傳同樣的訊息
+        line_bot_api.reply_message(tk,text_message)       # 回傳訊息
+    except:
+        print('error')
+    return 'OK'
 
-# In[ ]:
+#if __name__ == "__main__":
+ #   app.run()
+#    run_with_ngrok(app)
+#    app.run(host='0.0.0.0', port=5000)
 
-
+import os
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
-
-
-# In[ ]:
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 
