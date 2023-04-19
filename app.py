4ffa46ac694227f18440adc5b98e27792f305eac
kant1223 <131114390+kant1223@users.noreply.github.com>
2023-04-20 01:03:36 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,39 +1,34 @@
 #!/usr/bin/env python
 # coding: utf-8
-import os
+#from flask_ngrok import run_with_ngrok   # colab 使用，本機環境請刪除
 secret="575a9bb8ae3a9aa680e0dfb69f5f2750"
 access_token='8zp872p8zjIBjP89c4dtoPewB239ZuOuA62gIShbHcMuBBvssLb8f2fep9rFmVMGh5TBVgoo55L7S0KDYzNgy0+NGvmieYfx7DwunGMlmKefVPjWo0MPrpS3mMDCoAagqJCHIhbKoTRMjbbhx8BTKwdB04t89/1O/w1cDnyilFU=/'
-from flask import Flask
+from flask import Flask, request
 from linebot import LineBotApi, WebhookHandler
+from linebot.models import TextSendMessage   # 載入 TextSendMessage 模組
+import json
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(access_token)
-handler = WebhookHandler(secret)
-
-# 利用 handler 處理 LINE 觸發事件
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token, TextSendMessage(text=f"Hello {line_bot_api.get_profile(event.source.user_id).display_name}!")
-    )
-
-# 利用 route 處理路由
-from flask import request, abort
-from linebot.exceptions import InvalidSignatureError
-
-@app.route("/callback", methods=['POST'])
-def callback():
-    signature = request.headers['X-Line-Signature']
-
+@app.route("/kevin", methods=['POST'])
+def linebot():
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
+    json_data = json.loads(body)
+    print(json_data)
     try:
+        line_bot_api = LineBotApi(secret)
+        handler = WebhookHandler(access_token)
+        signature = request.headers['X-Line-Signature']
         handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
+        tk = json_data['events'][0]['replyToken']         # 取得 reply token
+        msg = json_data['events'][0]['message']['text']   # 取得使用者發送的訊息
+        text_message = TextSendMessage(text=msg)          # 設定回傳同樣的訊息
+        line_bot_api.reply_message(tk,text_message)       # 回傳訊息
+    except:
+        print('error')
     return 'OK'
+
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
