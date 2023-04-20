e687a450e8c76e63c7d2088005f851283f1add9f
kant1223 <131114390+kant1223@users.noreply.github.com>
2023-04-20 11:01:33 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python
 # coding: utf-8
-secret="16437d49173d8a5043820f072f160cf4"
-access_token='NiC8uLxdsG70iFc1wEBeJNWBvmLqF5U1QrdyeCg6bqf097PKDebdJy+c+eD6lDP5VXWn0qAh92vvK+x/hCstQYms3zO9qkzkzXE0865iuzAGgiFvikSPuQwbARmdDkVmka3/Fc9Cd4jH/qVP7SlIgQdB04t89/1O/w1cDnyilFU='
 #from flask_ngrok import run_with_ngrok   # colab 使用，本機環境請刪除
 from flask import Flask, request
 from linebot import LineBotApi, WebhookHandler
 from linebot.models import TextSendMessage   # 載入 TextSendMessage 模組
 import json
+import os
 
 app = Flask(__name__)
 
 @app.route("/", methods=['POST'])
 def linebot():
-    body = request.get_data(as_text=True)
+   # handler = WebhookHandler(os.getenv(secret))
+    #body = request.get_data(as_text=True)
     json_data = json.loads(body)
     print(json_data)
     try:
-        line_bot_api = LineBotApi(access_token)              # 確認 token 是否正確
-        handler = WebhookHandler(secret)                     # 確認 secret 是否正確
+        line_bot_api = LineBotApi(os.getenv('access_token'))
+  #      line_bot_api = LineBotApi(access_token)
+        handler = WebhookHandler(os.getenv('secret')
         signature = request.headers['X-Line-Signature']
+        body = request.get_data(as_text=True)
+        print("AAAAAAA",request.headers)
         handler.handle(body, signature)
         tk = json_data['events'][0]['replyToken']         # 取得 reply token
         msg = json_data['events'][0]['message']['text']   # 取得使用者發送的訊息
@@ -30,6 +33,6 @@ def linebot():
 
 import os
 if __name__ == "__main__":
-   # port=5000
+#    port=5000
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
